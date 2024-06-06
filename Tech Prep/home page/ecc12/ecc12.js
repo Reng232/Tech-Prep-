30183118802fb709231ea2d36cfe33ecfa1e0f19
Reng232 <124182642+Reng232@users.noreply.github.com>
2024-06-06 08:37:26 -0400

    Delete Tech Prep directory

--- Tech Prep/home page/ecc12/ecc12.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var video = document.querySelector('.vid');
-  video.addEventListener('ended', showArrow);
-});
-
-function showArrow() {
-  var arrow = document.querySelector('.scroll-down-arrow');
-  arrow.classList.remove('hide-arrow');
-}
